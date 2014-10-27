41eb2691e02aa1d82d988b2a7e7d5a199b35ae2f
Firstname Lastname <your_email@youremail.com>
2014-10-28 00:17:20 +0900

    add MyApp6.java

---
+++ MyApp6.java
@@ -0,0 +1,12 @@
+package ex6;
+
+public class MyApp6 {
+    /**
+     * 各数字の出現数を表す文字列を返す
+     * @param inputs　数列の文字列。例えば"1 3 1 1 3 3"
+     * @return　各数字の出現数を表す文字列。例えば"1(3) 3(3)"
+     */
+    public String count(String inputs) {
+        return null;
+    }
+}
